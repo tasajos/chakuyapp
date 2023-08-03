4d26a7237a5b23ffed14e99e70edc05f2c6ad79a
tasajos <tasajos@gmail.com>
2023-08-03 15:58:12 -0400

    update

--- app/src/main/java/org/chakuy/chakuy/ui/gallery/GalleryFragment.java
+++ app/src/main/java/org/chakuy/chakuy/ui/gallery/GalleryFragment.java
@@ -1,37 +1,192 @@
 package org.chakuy.chakuy.ui.gallery;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.chakuy.chakuy.R;
 import org.chakuy.chakuy.databinding.FragmentGalleryBinding;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends Fragment implements OnMapReadyCallback {
 
-    private FragmentGalleryBinding binding;
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
+    private EditText nombre, descripcion, ubicacion, fecha;
+    private Button btnalarma;
+
+    private ImageButton btn_registrar;
+    private Spinner tipoSpinner;
+
+    private FirebaseFirestore mfirestore;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location lastKnownLocation;
+    private GoogleMap googleMap;
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
-
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        FragmentGalleryBinding binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        mfirestore = FirebaseFirestore.getInstance();
+
+        // Find views by their IDs
+        nombre = root.findViewById(R.id.nombre);
+        tipoSpinner = root.findViewById(R.id.tipo);
+        descripcion = root.findViewById(R.id.descripcion);
+        ubicacion = root.findViewById(R.id.ubicacion);
+        fecha = root.findViewById(R.id.fecha);
+        btn_registrar = root.findViewById(R.id.btn_registrar);
+        btnalarma = root.findViewById(R.id.btnalarma);
+
+        // Set up the spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(),
+                R.array.tipos_producto, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tipoSpinner.setAdapter(adapter);
+
+        // Set up the click listeners
+        btnalarma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = ubicacion.getText().toString();
+                // Envía mensajes de WhatsApp si es necesario
+                //sendWhatsAppMessages(Arrays.asList("+59177087685", "+59163962491"), message);
+            }
+        });
+
+        btn_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nombre.getText().toString().trim();
+                String type = tipoSpinner.getSelectedItem().toString().trim();
+                String quantity = descripcion.getText().toString().trim();
+                String locationValue = ubicacion.getText().toString().trim();
+                String date = fecha.getText().toString().trim();
+
+                if (name.isEmpty() || type.isEmpty() ||  quantity.isEmpty() ||
+                        locationValue.isEmpty() || date.isEmpty()) {
+                    Toast.makeText(requireContext(), "Ingresar todos los datos", Toast.LENGTH_SHORT).show();
+                } else {
+                    saveRecord(name, type, quantity, locationValue, date);
+                }
+            }
+        });
+
+        // Obtener fecha actual
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String currentDate = sdf.format(new Date());
+
+        // Establecer fecha actual en EditText y desactivar edición
+        fecha.setText(currentDate);
+        fecha.setEnabled(false);
+
+        ubicacion.setEnabled(false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView2);
+        mapFragment.getMapAsync(this);
+
         return root;
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+        updateMapLocation();
+    }
+
+    private void updateMapLocation() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_CODE);
+        } else {
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                lastKnownLocation = location;
+                                ubicacion.setText(location.getLatitude() + ", " + location.getLongitude());
+                                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                googleMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+                                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f));
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void saveRecord(String name, String type, String quantity, String locationValue, String date) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("nombre", name);
+
+        map.put("tipo", type);
+
+        map.put("cantidad", quantity);
+
+        map.put("ubicacion", locationValue);
+        map.put("fecha", date);
+
+        mfirestore.collection("chakuydb").add(map)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(requireContext(), "Registro Exitoso", Toast.LENGTH_SHORT).show();
+                        clearForm(); // Limpia el formulario después de guardar
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(requireContext(), "Error al Ingresar", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void clearForm() {
+        // Restablecer los campos de texto a cadenas vacías
+        nombre.setText("");
+        descripcion.setText("");
+        ubicacion.setText("");
+
+
+        // Restablecer el spinner a la posición 0 (primer elemento)
+        tipoSpinner.setSelection(0);
+
+        // Actualizar el mapa para mostrar la ubicación actual
+        updateMapLocation();
     }
 }
--- app/src/main/java/org/chakuy/chakuy/ui/home/HomeFragment.java
+++ app/src/main/java/org/chakuy/chakuy/ui/home/HomeFragment.java
@@ -1,37 +1,170 @@
 package org.chakuy.chakuy.ui.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import org.chakuy.chakuy.databinding.FragmentHomeBinding;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.chakuy.chakuy.R;
+//import org.chakuy.chakuy.login;
+//import org.chakuy.chakuy.ui.verlista.slideshow.hbdbdata;
+//import org.chakuy.chakuy.ui.verlista.slideshow.vlista;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private LinearLayout linearLayoutAgregar;
+    private LinearLayout listado;
+    private LinearLayout descargar;
+    private LinearLayout cerrarsesion;
+
+    private LinearLayout listadopc;
+
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference hbdbRef = db.collection("chakuydb");
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        linearLayoutAgregar = root.findViewById(R.id.linearLayoutAgregar);
+        listado = root.findViewById(R.id.listado);
+        descargar = root.findViewById(R.id.descargar);
+        cerrarsesion = root.findViewById(R.id.cerrarsesion);
+        listadopc = root.findViewById(R.id.listadopc);
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        setupCardClickListeners();
+
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private void setupCardClickListeners() {
+        linearLayoutAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navegar a GalleryFragment
+                Navigation.findNavController(view).navigate(R.id.nav_gallery);
+            }
+        });
+
+
+  //*      listado.setOnClickListener(new View.OnClickListener() {
+     //       @Override
+        //    public void onClick(View view) {
+                // Acción para la tarjeta "Listado"
+                // Aquí puedes implementar el comportamiento deseado
+          //      Navigation.findNavController(view).navigate(R.id.nav_lista);
+            //    Toast.makeText(getActivity(), "Cargando Listado.....", Toast.LENGTH_SHORT).show();
+            //}
+        //});
+
+        //listadopc.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+            //public void onClick(View view) {
+                // Acción para la tarjeta "Listado PC"
+                //Intent intent = new Intent(getActivity(), DatosPC.class);
+
+              //  Navigation.findNavController(view).navigate(R.id.nav_pc);
+            //}
+        //});
+        //descargar.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+            //public void onClick(View view) {
+                // Acción para la tarjeta "Descargar"
+                // Aquí puedes implementar el comportamiento deseado
+
+              //  List<chakuydb> dataList = new ArrayList<>();
+                //Query query = hbdbRef;
+                //query.get().addOnSuccessListener(queryDocumentSnapshots -> {
+                  //  for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                    //    chakuydb data = document.toObject(chakuydb.class);
+                      //  dataList.add(data);
+                    //}
+
+                    //Workbook workbook = new XSSFWorkbook();
+                    //Sheet sheet = workbook.createSheet("Datos MTP");
+
+                    //int rowNum = 0;
+                    //for (chakuydb data : dataList) {
+                      //  Row row = sheet.createRow(rowNum++);
+
+                        //row.createCell(0).setCellValue(data.getArea());
+                        //row.createCell(1).setCellValue(data.getDescripcion());
+                        //row.createCell(2).setCellValue(data.getEstado());
+                        //row.createCell(3).setCellValue(data.getNombre());
+                        //row.createCell(4).setCellValue(data.getFecha());
+
+                    //}
+
+                    //String filePath = getExternalFilesDir(null).getPath().toString() + "/datos_hbdb.xlsx";
+                    //String filePath = requireActivity().getExternalFilesDir(null).getPath() + "/datos_registros.xlsx";
+                    //try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
+                      //  workbook.write(outputStream);
+                    //} catch (IOException e) {
+                      //  e.printStackTrace();
+                    //}
+
+                    //Uri fileUri = FileProvider.getUriForFile(requireContext(), requireContext().getApplicationContext().getPackageName() + ".provider", new File(filePath));
+
+                    //Intent intent = new Intent(Intent.ACTION_SEND);
+                    //intent.setType("application/vnd.ms-excel");
+                    //intent.putExtra(Intent.EXTRA_STREAM, fileUri);
+                    //intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    //startActivity(Intent.createChooser(intent, "Compartir archivo"));
+
+                //});
+
+
+
+                //Toast.makeText(getActivity(), "Descargando....espera unos segundos", Toast.LENGTH_SHORT).show();
+            //}
+        //});
+
+        //cerrarsesion.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+            //public void onClick(View view) {
+
+
+                // Acción para la tarjeta "Cerrar Sesión"
+              //  logout();
+            //}
+    //    });
     }
+
+    //private void logout() {
+      //  FirebaseAuth.getInstance().signOut();
+        //Toast.makeText(getActivity(), "Cierre de sesión exitoso", Toast.LENGTH_SHORT).show();
+        //startActivity(new Intent(getActivity(), login.class));
+        //getActivity().finish();
+    //}
 }
